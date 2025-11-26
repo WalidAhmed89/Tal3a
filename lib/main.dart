0f9fa6cd133c66ec0e4c85ce26b3eaeecf1e07ee
Mohaned <mhndwayl47@gmail.com>
2025-11-26 18:37:04 +0200

    by mohaned

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'kosomk',
+      title: 'ksosa',
       theme: ThemeData(
         // This is the theme of your application.
         //
